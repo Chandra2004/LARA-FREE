73dff31ab807f174e179e008c037f89c8b964541
ccgraphics2001 <ccgraphics2001@gmail.com>
2025-08-13 11:16:49 +0000

    M: Update index

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,11 @@
-<?php
-// Arahkan semua request ke public/index.php Laravel
-require __DIR__ . '/../public/index.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    hello anjir
+</body>
+</html>
